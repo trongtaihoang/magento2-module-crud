399d58591dcb48bebb5459cbdc188db075f42076
Trong Tai <hoangtrongtaitb95@gmail.com>
2018-10-16 22:49:33 +0700

    add massDelete

--- Model/ResourceModel/Teacher/Collection.php
+++ Model/ResourceModel/Teacher/Collection.php
@@ -8,8 +8,31 @@ use Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
  */
 class Collection extends AbstractCollection
 {
+	protected $_idFieldName = 'teacher_id';
+
+    /**
+     * Load data for preview flag
+     *
+     * @var bool
+     */
+    protected $_previewFlag;
+
+    /**
+     * Event prefix
+     *
+     * @var string
+     */
+    protected $_eventPrefix = 'crud_teacher_collection';
+
+    /**
+     * Event object
+     *
+     * @var string
+     */
+    protected $_eventObject = 'teacher_collection';
 	function _construct()
 	{
 		$this->_init(\Aht\HttCrud\Model\teacher::class,\Aht\HttCrud\Model\ResourceModel\teacher::class);
+		$this->_map['fields']['teacher_id'] = 'main_table.teacher_id';
 	}
 }
